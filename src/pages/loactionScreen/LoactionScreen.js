638333f6e1f9244e204af7e21436c5e1375e3368
Saurav Kumar <kumarsaurav26198@gmail.com>
2022-09-14 10:22:01 +0530

    add redux thunk

--- src/pages/loactionScreen/LoactionScreen.js
+++ src/pages/loactionScreen/LoactionScreen.js
@@ -1,12 +1,32 @@
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
+import CustomButton from '../../components/common/CustomButton';
+import { WebView } from 'react-native-webview';
 
-export default function LoactionScreen() {
+const LoactionScreen = ({ navigation }) => {
     return (
-        <View>
-            <Text>LoactionScreen</Text>
-        </View>
+        <>
+            <View>
+            </View>
+            <View style={styles.Maplocation}>
+                <WebView source={{ html: '<iframe width="100%" height="100%"  scrolling="no"  src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Parkview%20Residence%20Colony,%20Sector%2066,%20Sahibzada%20Ajit%20Singh%20Nagar,%20Punjab%20160066+(Your%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.gps.ie/sport-gps/">swimming watch</a></iframe>' }} />
+                <View style={styles.backButtonContainer}>
+                    <CustomButton title={"confirm  loaction"} onPress={() => navigation.navigate("ShoppingCartScreen")} />
+                </View>
+            </View>
+        </>
     );
-}
+};
 
-const styles = StyleSheet.create({});
+export default LoactionScreen;
+
+const styles = StyleSheet.create({
+    Maplocation: {
+        width: "100%",
+        height: "100%"
+    },
+    backButtonContainer: {
+        bottom: 4,
+        marginHorizontal: 10
+    },
+});
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,30 +1,9 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import reducers from "../redux/reducers";
+import thunk from 'redux-thunk';
+
 
 const configureStore = () => {
-    return createStore(reducers);
+    return createStore(reducers, applyMiddleware(thunk));
 };
 export default configureStore;
-
-
-// import { configureStore } from "@reduxjs/toolkit";
-// import reducers from "../redux/reducers";
-
-// import { createStore } from 'redux';
-
-// const configureStore = () => {
-//     return createStore(reducers);
-// };
-// export default configureStore;
-
-// import { configureStore } from "@reduxjs/toolkit";
-// import reducers from "../redux/reducers";
-// // import consentSaga from "../redux/reducers/consentSaga";
-// // import createSagaMiddleware from 'redux-saga';
-
-// // const sagaMiddleware = createSagaMiddleware();
-// export const store = configureStore({
-//     reducer: reducers,
-//     // middleware: () => [sagaMiddleware]
-// });
-// // sagaMiddleware.run(consentSaga);
