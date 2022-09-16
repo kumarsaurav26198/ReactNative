3fc452af44224125eb752a79220c3ac9edef2d26
Saurav Kumar <kumarsaurav26198@gmail.com>
2022-09-16 10:29:42 +0530

    add native firebase with setup

--- src/beforeLogIn/screens/index.js
+++
@@ -1,9 +0,0 @@
-import { WelcomeScreen } from "./WelcomeScreen";
-import { LoginScreen } from "./LoginScreen";
-import { SignupScreen } from "./SignupScreen";
-
-export {
-    SignupScreen,
-    LoginScreen,
-    WelcomeScreen
-};
